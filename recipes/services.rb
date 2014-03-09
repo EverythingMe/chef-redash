66f514bd2e3a91ba4ede645968f9838136bc7b53
Arik Fraimovich <arik@arikfr.com>
2014-03-09 11:22:46 +0200

    Enable updater service

--- recipes/services.rb
+++ recipes/services.rb
@@ -5,7 +5,7 @@ runit_service "redash-server" do
   subscribes :restart, "ark[redash]"
 end
 
-# runit_service "redash-workers" do
-  # subscribes :restart, "template[env.sh]"
-  # subscribes :restart, "ark[redash]"
-# end
+runit_service "redash-updater" do
+  subscribes :restart, "template[env.sh]"
+  subscribes :restart, "ark[redash]"
+end
