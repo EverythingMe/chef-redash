295aa60c7403fc4d0b3e7b2601f62331e0accde4
Arik Fraimovich <arik@arikfr.com>
2014-04-13 11:55:23 +0300

    Remove nginx site creation

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -1,19 +1,5 @@
 include_recipe "nginx"
 
-template "#{node['nginx']['dir']}/sites-available/redash" do
-  mode 0777
-  owner node['nginx']['user']
-  group node['nginx']['user']
-end
-
 nginx_site 'default' do
   enable false
 end
-
-nginx_site "redash"
-
-# TODO: restart nginx only when needed
-service 'nginx' do
-  action :restart
-end
-
